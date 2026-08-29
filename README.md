8fdfc586cf1b762a01ee58caccc4ebfeeb1d9e8f
Jellyca Villanueva <jellycavillanueva07@gmail.com>
2026-08-29 10:52:09 +0800

    Initial commit - Laravel setup with homepage

---
+++ README.md
@@ -0,0 +1,6 @@
+# Client-Server Week 2 - Laravel Setup
+
+This project displays student information using Laravel framework.
+
+## Homepage Preview
+![Student Profile](screenshots/homepage.png)
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+    <h2>Student Profile</h2>
+    <ul>
+        <li><strong>Student Name:</strong> Jellyca Villanueva</li>
+        <li><strong>Student Number:</strong> 0124-1130</li>
+        <li><strong>Course:</strong> BS Information Technology</li>
+        <li><strong>Section:</strong> IT-3A</li>
+        <li><strong>Subject:</strong> Web Development</li>
+        <li><strong>Current Date:</strong> {{ date('F d, Y') }}</li>
+    </ul>
+</body>
+</html>
